a9caf90ac9b5c2878880c86538438918fc501a51
fateh0007 <fatehalam123463@gmail.com>
2026-02-10 19:18:57 +0530

    Completed todo controllers

--- backend/src/controllers/todo.controller.js
+++ backend/src/controllers/todo.controller.js
@@ -25,19 +25,79 @@ const createTodo = asyncHandler(async(req, res) => {
 })
 
 const getAllTodos = asyncHandler(async(req, res) => {
-    
+    const todos = await Todo.find({userId: req.user._id})
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, todos, "Todos fetched successfully")
+    )
 })
 
 const getTodoById = asyncHandler(async(req, res) => {
-    
+    const {id} = req.params;
+    const todo = await Todo.findOne({
+        _id: id,
+        userId: req.user._id
+    })
+    if(!todo){
+        throw new ApiError(404, "Todo not found");
+    }
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, todo, "Todo fetched successfully")
+    )
 })
 
 const updateTodo = asyncHandler(async(req, res) => {
+    const todoId = req.params.id;
+    const {title, description, completed} = req.body;
     
+    if(!title && !description && completed === undefined){
+        throw new ApiError(400, "At least one field is required")
+    }
+
+    const updateData = {};
+    if(title!==undefined) updateData.title = title;
+    if(description!==undefined) updateData.description = description;
+    if(completed !== undefined) updateData.completed = completed;
+
+    const todo = await Todo.findByIdAndUpdate(
+        {
+            _id: todoId,
+            userId: req.user._id
+        },
+        {
+            $set: updateData
+        },
+        {new: true}
+    )
+
+    if(!todo){
+        throw new ApiError(404,"Todo not found")
+    }
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200,todo, "Todo updated successfully")
+    )
 })
 
+
 const deleteTodo = asyncHandler(async(req, res) => {
-    
+    const todoId = req.params.id;
+    const todo = await Todo.findOneAndDelete({
+        _id: todoId,
+        userId: req.user._id
+    })
+    if(!todo){
+        throw new ApiError(404,"Todo not found")
+    }
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, null, "Todo deleted successfully")
+    )
 })
 
 export {createTodo, getAllTodos, getTodoById, updateTodo, deleteTodo}
