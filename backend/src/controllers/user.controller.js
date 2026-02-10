22d8c496196cd6eec9d373daaa4e59f0e1c8bc98
fateh0007 <fatehalam123463@gmail.com>
2026-02-10 16:08:57 +0530

    User registration Controller completed

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -4,3 +4,38 @@ import { User} from "../models/user.model.js"
 import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken"
 import mongoose from "mongoose";
+
+const registerUser = asyncHandler(async(req,res)=>{
+    const {username, email, fullName, password} = req.body
+
+    if([username, email, fullName, password].some(field => field?.trim() === "")){
+        throw new ApiError(400, "All fields are required")
+    }
+
+    const existedUser = await User.findOne({
+        $or: [{username}, {email}]
+    })
+
+    if(existedUser){
+        throw new ApiError(409, "User with email or username already exists")
+    }
+
+    const user = await User.create({
+        username: username.toLowerCase(),
+        email,
+        fullName,
+        password
+    })
+    
+    const createdUser = await User.findById(user._id).select("-password -refreshToken")
+    
+    if(!createdUser){
+        throw new ApiError(500, "Something went wrong while registering the user")
+    }
+    
+    return res.status(201).json(
+        new ApiResponse(200, createdUser, "User registered Successfully")
+    )
+})
+
+export {registerUser}
