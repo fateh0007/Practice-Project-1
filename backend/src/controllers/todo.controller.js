f9070d3d03de10bee86ce931e40ffe36e925640f
fateh0007 <fatehalam123463@gmail.com>
2026-02-10 18:45:11 +0530

    Created CreatTodo controller

--- backend/src/controllers/todo.controller.js
+++ backend/src/controllers/todo.controller.js
@@ -6,7 +6,22 @@ import mongoose from "mongoose";
 import {Todo} from "../models/todo.model.js"
 
 const createTodo = asyncHandler(async(req, res) => {
-    
+    const {title, description} = req.body
+
+    if(!title?.trim() || !description?.trim()){
+        throw new ApiError(400, "Title and description are required")
+    }
+
+    const todo = await Todo.create({
+        title,
+        description,
+        userId: req.user._id
+    })
+    return res
+    .status(201)
+    .json(
+        new ApiResponse(201, todo, "Todo created successfully")
+    )
 })
 
 const getAllTodos = asyncHandler(async(req, res) => {
