fad849be63199289e1fd7e3d109af1dd73ffd250
fateh0007 <fatehalam123463@gmail.com>
2026-02-11 12:33:44 +0530

    Completed Auth Context

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,3 +1,53 @@
-import { createContext } from "react";
+import { createContext, useState, useEffect} from "react";
+import {getCurrentUser, loginUser, logoutUser} from "../api/auth.api";
+
 
 export const AuthContext = createContext(null);
+
+export const AuthProvider = ({children}) => {
+    const [user,setUser] = useState(null);
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [loading, setLoading] = useState(true);
+    
+    useEffect(()=>{
+        const loadUser = async() => {
+            try {
+                const res = await getCurrentUser();
+                setUser(res.data.data); // 
+                setIsAuthenticated(true);
+            } catch (error) {
+                setUser(null);
+                setIsAuthenticated(false);
+            } finally {
+                setLoading(false);
+            }
+        }
+        loadUser();
+    }, [])
+
+    const login = async(data) =>{
+        const res = await loginUser(data);
+        setUser(res.data.data.user);
+        setIsAuthenticated(true);
+    }
+
+    const logout = async() => {
+        await logoutUser();
+        setUser(null);
+        setIsAuthenticated(false);
+    }
+
+    return (
+        <AuthContext.Provider 
+            value={{
+                user,
+                isAuthenticated,
+                loading,
+                login,
+                logout
+            }}
+        >
+            {children}
+        </AuthContext.Provider>
+    )
+}
