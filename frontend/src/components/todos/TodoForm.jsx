91a97bbfd0a668b91c39388e5357c03df4ed3e2e
fateh0007 <fatehalam123463@gmail.com>
2026-02-14 13:16:14 +0530

    Creating Components for hero section

---
+++ frontend/src/components/todos/TodoForm.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react"
+
+const TodoForm = () => {
+    const [title, setTitle] = useState("");
+    cosnt [description, setDescription] = useState("");
+    return (
+        <form>
+            Todo Form
+        </form>
+    )
+}
